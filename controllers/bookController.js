3581ab4bd1220fd9962e28d72d082cd16c3d0281
nycwy <stuxy.1129@gmail.com>
2026-01-01 23:58:59 +0545

    Read any specific book based on id

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -31,3 +31,21 @@ export const getAllBooks = async (req, res) => {
         res.status(500).json({ message: "Server Error" });
     }
 };
+
+export const getBookById = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const book = await Book.findById(id);
+
+        if (!book) {
+            return res.status(404).json({ message: "Book not found" });
+        }
+
+        res.status(200).json(book);
+
+    } catch (error) {
+        console.error("Error fetching book:", error);
+        res.status(500).json({ message: "Server Error" });
+    }
+};
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,10 +1,12 @@
 import express from "express"
-import { createBook, getAllBooks } from "../controllers/bookController.js"
+import { createBook, getAllBooks, getBookById } from "../controllers/bookController.js"
 
 const router = express.Router();
 
 router.post("/", createBook);
 
 router.get("/", getAllBooks);
+
+router.get("/:id", getBookById);
 
 export default router;
